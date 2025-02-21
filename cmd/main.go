2198fb68952d49a809c14625297fff8574166250
FLASH2332 <jayaveeran2005@gmail.com>
2025-02-21 22:25:37 +0530

    Implemented reverse proxy to direct requests to available targets and verified its working

--- cmd/main.go
+++ cmd/main.go
@@ -5,18 +5,20 @@ import (
 	"log"
 
 	"github.com/FLASH2332/novus-load-balancer/config"
+	"github.com/FLASH2332/novus-load-balancer/internal/proxy"
+	"net/http"
 )
 
 func main() {
 	// Load configuration
 	config.LoadConfig("config/config.yaml")
 
-	// Print loaded config values
-	fmt.Println("Server Port:", config.Cfg.Server.Port)
-	fmt.Println("Proxy Targets:", config.Cfg.Proxy.Targets)
-	fmt.Println("Cache Max Size:", config.Cfg.Cache.MaxSize)
-	fmt.Println("Load Balancer Strategy:", config.Cfg.LoadBalancer.Strategy)
+	// Create a new reverse proxy
+	reverseProxy := proxy.NewReverseProxy(config.Cfg.Proxy.Targets)
 
-	// Keep the application running
-	log.Println("Configuration loaded successfully!")
+	// Start the server
+	port := config.Cfg.Server.Port
+	fmt.Printf("Reverse Proxy running on port %d\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), reverseProxy))
+
 }
